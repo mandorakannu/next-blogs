646bfdc18678f7273607c0eb24077cc9efa96d44
mandorakannu <mandorakannu@gmail.com>
2024-11-21 20:39:55 +0530

    Add ScrollToTop component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hamburger } from "@ui/Hambar.Modal";
 import { Header } from "@shared_ui/Nav/Header";
+import { ScrollToTop } from "@ui/ScrollToTop";
 
 export default async function Home() {
   return (
     <>
       <Header />
       <Hamburger />
+      <ScrollToTop />
     </>
   )
 }
---
+++ components/ui/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { CircularProgress } from "@nextui-org/react";
+export const ScrollToTop = () => {
+
+    const [value, setValue] = useState<number>(0);
+    useEffect(() => {
+        const handleScroll = (): void => {
+            const scrollY = Math.floor(window.scrollY);
+            const height = Math.floor(window.document.body.offsetHeight - window.innerHeight);
+            console.log("Scroll Y: ", scrollY);
+            console.log("height: ", height);
+            setValue(Math.round(scrollY / (height!) * 100));
+        }
+        window.addEventListener("scroll", () => {
+            handleScroll();
+        });
+    }, []);
+
+
+    return (
+        <>
+            <CircularProgress
+                label=""
+                size="lg"
+                value={value}
+                color="secondary"
+                formatOptions={{ style: "percent" }}
+                showValueLabel={true}
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                className="items-center justify-center fixed bottom-4 right-4 z-50 cursor-pointer bg-white rounded-full shadow-lg"
+            />
+        </>
+    )
+};
