8eef4a8b55675925986b1906c43c5a8c403a9a5d
mandorakannu <mandorakannu@gmail.com>
2024-11-21 18:55:44 +0530

    Hamburger created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Hamburger } from "@ui/Hambar.Modal";
 import { Header } from "@shared_ui/Nav/Header";
 
 export default async function Home() {
   return (
     <>
       <Header />
+      <Hamburger />
     </>
   )
 }
