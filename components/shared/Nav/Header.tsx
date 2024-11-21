32867a963cb49be421fb5cdc0ebbdbff68998e13
mandorakannu <mandorakannu@gmail.com>
2024-11-21 18:58:39 +0530

    Fix Responsiveness

--- components/shared/Nav/Header.tsx
+++ components/shared/Nav/Header.tsx
@@ -7,9 +7,11 @@ export const Header = () => {
     return (
         <>
             <div className="flex flex-row justify-evenly items-center my-6">
-                <SocialMedia iconSize={15} classProps="gap-2" />
-                <h1 className="text-3xl uppercase font-serif cursor-default">Journal</h1>
-                <HeaderModal />
+                <SocialMedia iconSize={15} classProps="gap-2 max-sm:hidden" />
+                <h1 className="max-sm:text-4xl text-3xl uppercase font-serif cursor-default">Journal</h1>
+                <div className="max-sm:hidden">
+                    <HeaderModal />
+                </div>
             </div>
         </>
     )
