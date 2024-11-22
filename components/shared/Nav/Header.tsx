63bd3f8883d28bac0ede4ed488baed0d3bcc8765
mandorakannu <mandorakannu@gmail.com>
2024-11-22 19:57:12 +0530

    Add Oswald font to Header and adjust styles in Hamburger component

--- components/shared/Nav/Header.tsx
+++ components/shared/Nav/Header.tsx
@@ -1,6 +1,11 @@
 import { HeaderModal } from "@ui/Header.Modal"
 import { SocialMedia } from "./SocialMedia"
+import { Oswald } from "next/font/google"
 
+const font = Oswald({
+    display: "swap",
+    weight: ["200", "300", "400", "500", "600", "700"]
+})
 
 export const Header = () => {
 
@@ -8,7 +13,7 @@ export const Header = () => {
         <>
             <div className="flex flex-row justify-evenly items-center my-6">
                 <SocialMedia iconSize={15} classProps="gap-2 max-sm:hidden" />
-                <h1 className="max-sm:text-4xl text-3xl uppercase font-serif cursor-default">Journal</h1>
+                <h1 className={`max-sm:text-4xl text-3xl uppercase cursor-default ${font.className} tracking-widest`}>Journal</h1>
                 <div className="max-sm:hidden">
                     <HeaderModal />
                 </div>
--- components/ui/Hambar.Modal.tsx
+++ components/ui/Hambar.Modal.tsx
@@ -2,25 +2,20 @@
 
 import { Link } from "@nextui-org/react";
 import { CiSearch } from "react-icons/ci";
-import { Open_Sans } from "next/font/google"
 import { HeaderModal } from "./Header.Modal";
 
-const font = Open_Sans({
-    weight: ["300", "400", "500", "600", "700", "800"],
-    display: "swap",
-});
 
 export const Hamburger = () => {
 
     return (
         <>
-            <div className="flex sm:justify-evenly max-sm:justify-center items-center gap-8 sm:border-y-2 sm:border-gray-500 max-sm:border-b-2 max-sm:border-black py-6 max-sm:mx-10 sm:mx-20">
+            <div className="flex sm:justify-evenly max-sm:justify-center items-center gap-8 sm:border-y-2 sm:border-gray-500 max-sm:border-b-2 max-sm:border-black pb-4 max-sm:mx-10 sm:mx-20">
                 <div className="flex flex-col justify-center items-center gap-1 group">
                     <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
                     <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
                     <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
                 </div>
-                <ul className={`flex flex-row justify-evenly items-center gap-8 uppercase font-medium max-sm:hidden ${font.className}`}>
+                <ul className="flex flex-row justify-evenly items-center gap-8 uppercase font-medium max-sm:hidden">
                     <Link size="lg" href="/" key="Home" className="hover:underline underline-offset-2">Home</Link>
                     <Link size="lg" href="/about" key="About" className="hover:underline underline-offset-2">About</Link>
                     <Link size="lg" href="/features" key="Features" className="hover:underline underline-offset-2">Features</Link>
@@ -28,7 +23,7 @@ export const Hamburger = () => {
                     <Link size="lg" href="/contact" key="Contact" className="hover:underline underline-offset-2">Contact</Link>
                 </ul>
                 <CiSearch className="text-3xl cursor-pointer" />
-                <div className="sm:hidden"><HeaderModal/></div>
+                <div className="sm:hidden"><HeaderModal /></div>
             </div>
         </>
     );
