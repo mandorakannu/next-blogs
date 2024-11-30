ac44c90fdcd3b5ced0d1197cf2900bf5036fb1db
mandorakannu <mandorakannu@gmail.com>
2024-11-30 11:23:16 +0530

    fix: add crawlDelay to robots file for improved crawling control

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
       disallow: '/studio/',
+      crawlDelay: 10,
     },
     sitemap: 'https://content.mandorakannu.xyz/sitemap.xml',
   }
