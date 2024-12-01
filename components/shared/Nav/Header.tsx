1fbdd25500df819c2a76ebf20cf03abb5208775d
mandorakannu <mandorakannu@gmail.com>
2024-12-01 23:38:13 +0530

    Remove: Search btn & SignUp btn

--- components/shared/Nav/Header.tsx
+++ components/shared/Nav/Header.tsx
@@ -1,24 +1,43 @@
-import { HeaderModal } from "@ui/Header.Modal"
-import { SocialMedia } from "./SocialMedia"
-import { Oswald } from "next/font/google"
+"use client";
+import { SocialMedia } from "./SocialMedia";
+import { Oswald } from "next/font/google";
+import { Drawer } from "@shared_ui/Nav/Drawer";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const font = Oswald({
-    display: "swap",
-    weight: ["200", "300", "400", "500", "600", "700"],
-    subsets: ["latin", "latin-ext"],
-})
+  display: "swap",
+  weight: ["200", "300", "400", "500", "600", "700"],
+  subsets: ["latin", "latin-ext"],
+});
 
 export const Header = () => {
-
-    return (
-        <>
-            <div className="flex flex-row justify-evenly items-center my-6">
-                <SocialMedia iconSize={15} classProps="gap-2 max-sm:hidden" />
-                <h1 className={`max-sm:text-4xl text-3xl uppercase cursor-default ${font.className} tracking-widest`}>Journal</h1>
-                <div className="max-sm:hidden">
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  return (
+    <>
+      {pathname.includes("/studio") ? null : (
+        <div className="flex flex-row justify-evenly items-center my-6">
+          <SocialMedia iconSize={15} classProps="gap-2 max-sm:hidden" />
+          <h1
+            className={`max-sm:text-4xl text-3xl uppercase cursor-default ${font.className} tracking-widest`}
+          >
+            Journal
+          </h1>
+          <div
+            className="flex flex-col justify-center items-center gap-1 group"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
+            <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
+            <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
+          </div>
+          <Drawer handleFxn={() => setIsOpen(!isOpen)} isOpen={isOpen} />
+          {/* <div className="max-sm:hidden">
                     <HeaderModal />
-                </div>
-            </div>
-        </>
-    )
-}
+                </div> */}
+        </div>
+      )}
+    </>
+  );
+};
--- components/ui/Hambar.Modal.tsx
+++ components/ui/Hambar.Modal.tsx
@@ -1,26 +1,25 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Link } from "@nextui-org/react";
-import { CiSearch } from "react-icons/ci";
-import { HeaderModal } from "./Header.Modal";
-import { Drawer } from "@shared_ui/Nav/Drawer";
+// import { CiSearch } from "react-icons/ci";
+// import { HeaderModal } from "./Header.Modal";
+// import { Drawer } from "@shared_ui/Nav/Drawer";
 
 export const Hamburger = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   return (
     <>
       {pathname.includes("/studio") ? null : (
         <div className="flex sm:justify-evenly max-sm:justify-center items-center gap-8 sm:border-y-2 sm:border-gray-500 max-sm:border-b-2 max-sm:border-black py-3 max-sm:mx-10 sm:mx-20 mb-10">
-          <div
+          {/* <div
             className="flex flex-col justify-center items-center gap-1 group"
             onClick={() => setIsOpen(!isOpen)}
           >
             <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
             <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
             <div className="w-8 h-1 bg-black group-hover:bg-primary-400 delay-75 transition-colors cursor-pointer"></div>
-          </div>
+          </div> */}
           <ul className="flex flex-row justify-evenly items-center gap-8 uppercase font-medium max-sm:hidden">
             <Link
               size="lg"
@@ -63,11 +62,11 @@ export const Hamburger = () => {
               Contact
             </Link>
           </ul>
-          <CiSearch className="text-3xl cursor-pointer" />
-          <div className="sm:hidden">
+          {/* <CiSearch className="text-3xl cursor-pointer" /> */}
+          {/* <div className="sm:hidden">
             <HeaderModal />
-          </div>
-          <Drawer isOpen={isOpen} handleFxn={() => setIsOpen(!isOpen)} />
+          </div> */}
+          {/* <Drawer isOpen={isOpen} handleFxn={() => setIsOpen(!isOpen)} /> */}
         </div>
       )}
     </>
